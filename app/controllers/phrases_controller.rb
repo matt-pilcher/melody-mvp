faf542dee922f9a96c74acfbc26e5998a7e05e9f
matt-pilcher <mpilcher90@gmail.com>
2022-04-25 15:30:04 -0400

    dynamically update Phrase data with no page refresh

--- app/controllers/phrases_controller.rb
+++ app/controllers/phrases_controller.rb
@@ -3,7 +3,8 @@ class PhrasesController < ApplicationController
     @song = Song.find(params[:song_id])
     @phrase = @song.phrases.create(phrase_params)
 
-    redirect_to song_path(@song)
+    turbo_stream
+    # redirect_to song_path(@song)
   end
 
   def edit
