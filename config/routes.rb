9df046fbac91e3afc33e1a390d6a586015221edd
matt-pilcher <mpilcher90@gmail.com>
2022-04-26 12:14:07 -0400

    add login functionality and auth redirects

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
 
   get 'register', to: 'user_registrations#new'
   post 'register/user', to: 'user_registrations#create'
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'login' => "user_sessions#create"
 
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
